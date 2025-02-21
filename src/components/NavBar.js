f67dab791ac35013840e5cfc22d2c710ae370f75
SumitRodrigues <sumitrod11@gmail.com>
2025-02-21 09:50:35 -0800

    updated leetcode icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
-import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, SunIcon, MoonIcon } from './Icons'
+import { TwitterIcon, LeetCodeIcon, GithubIcon, LinkedInIcon, PinterestIcon, SunIcon, MoonIcon } from './Icons'
 import { motion } from "framer-motion"
 import useThemeSwitcher from './hooks/useThemeSwitcher'
 
@@ -58,7 +58,6 @@ const NavBar = () => {
         setIsOpen(!isOpen)
     }
 
-
     return (
         <header
             className='w-full px-32 py-8 font-medium flex items-center justify-between text-dark
@@ -75,7 +74,6 @@ const NavBar = () => {
                     <CustomLink href="/" title="Home" className='mr-4' />
                     <CustomLink href="/about" title="About" className='mx-4' />
                     <CustomLink href="/projects" title="Projects" className='mx-4' />
-                    {/* <CustomLink href="/articles" title="Articles" className='ml-4' /> */}
                 </nav>
 
                 <nav className='flex items-center justify-center flex-wrap'>
@@ -103,11 +101,11 @@ const NavBar = () => {
                     >
                         <PinterestIcon />
                     </motion.a>
-                    <motion.a href="https://dribbble.com/SumitRodrigues" target={"_blank"} className='w-6 mx-3'
+                    <motion.a href="https://leetcode.com/SumitRodrigues/" target={"_blank"} className='w-6 mx-3'
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
                     >
-                        <DribbbleIcon />
+                        <LeetCodeIcon />
                     </motion.a>
 
                     <button
@@ -119,69 +117,59 @@ const NavBar = () => {
 
                 </nav>
             </div>
- 
 
             {
                 isOpen ?
 
-                <motion.div
-                initial={{scale:0, opacity:0, x: "-50%", y: "-50%"}}
-                animate={{scale:1, opacity:1}}
+                    <motion.div
+                        initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+                        animate={{ scale: 1, opacity: 1 }}
 
-                className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                        className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
             bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
             '>
                         <nav className='flex items-center flex-col justify-center'>
                             <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
                             <CustomMobileLink href="/about" title="About" className='' toggle={handleClick} />
                             <CustomMobileLink href="/projects" title="Projects" className='' toggle={handleClick} />
-                            {/* <CustomLink href="/articles" title="Articles" className='' toggle={handleClick}  /> */}
                         </nav>
 
                         <nav className='flex items-center justify-center flex-wrap mt-2'>
-                            <motion.a href="https://twitter.com/sumit_rodr88571" target={"_blank"} 
-                            className='w-6 mx-3 sm:mx-1'
+                            <motion.a href="https://twitter.com/sumit_rodr88571" target={"_blank"}
+                                className='w-6 mx-3 sm:mx-1'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
                             >
                                 <TwitterIcon />
                             </motion.a>
-                            <motion.a href="https://github.com/SumitRodrigues" target={"_blank"} 
-                            className='w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1'
+                            <motion.a href="https://github.com/SumitRodrigues" target={"_blank"}
+                                className='w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
                             >
                                 <GithubIcon />
                             </motion.a>
-                            <motion.a href="https://www.linkedin.com/in/sumit-rodrigues/" target={"_blank"} 
-                            className='w-6 mx-3 sm:mx-1'
+                            <motion.a href="https://www.linkedin.com/in/sumit-rodrigues/" target={"_blank"}
+                                className='w-6 mx-3 sm:mx-1'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
                             >
                                 <LinkedInIcon />
                             </motion.a>
-                            <motion.a href="https://in.pinterest.com/sumitrod11/" target={"_blank"} 
-                            className='w-6 mx-3 bg-light rounded-full sm:mx-1'
+                            <motion.a href="https://in.pinterest.com/sumitrod11/" target={"_blank"}
+                                className='w-6 mx-3 bg-light rounded-full sm:mx-1'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
                             >
                                 <PinterestIcon />
                             </motion.a>
-                            <motion.a href="https://dribbble.com/SumitRodrigues" target={"_blank"} 
-                            className='w-6 mx-3 sm:mx-1'
+                            <motion.a href="https://leetcode.com/u/sumitrod11/" target={"_blank"}
+                                className='w-6 mx-3 sm:mx-1'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
                             >
-                                <DribbbleIcon />
+                                <LeetCodeIcon />
                             </motion.a>
-
-                            <button
-                                onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                                className={`ml-3 flex items-center justify-center rounded-full p-1 sm:mx-1
-                        ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
-                                {mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />}
-                            </button>
-
                         </nav>
                     </motion.div>
 
@@ -195,4 +183,4 @@ const NavBar = () => {
     )
 }
 
-export default NavBar
+export default NavBar;
